087b22957aa0fd45fec40b417f6d515bec7ad66e
centipeda <joshua.c.guam@gmail.com>
2016-10-24 23:11:15 +1000

    added intial verification of repo when adding to list

--- github.py
+++ github.py
@@ -6,6 +6,7 @@
 # issue submission directly from Discord
 
 import gitpython
+import github3
 import discord
 from discord.ext import commands
 
@@ -13,6 +14,21 @@ class GitHub(object):
 
     def __init__(self, bot):
         self.bot = bot
+        # set up permanant storage for assigned repos later
+        self.repos = [ # "centipeda/gh-cog-test-repo"
+                     ]
+
+    @commands.command()
+    async def addrepo(self, owner: str, repo: str):
+        """Adds a repository to the set of repos to be checked regularly, first checking if it is a valid/accessible repo."""
+        try:
+            repo = github3.repository(owner, repo)
+        except exception as e:
+            await self.bot.reply("Failed to add repository.")
+            await self.bot.say("{}: {}".format(e.__name__,e))
+        else:
+            await self.bot.say("Repository verified. Adding to list of sources.")
+            self.repos.append("/".join((owner, repo)))
 
 def setup(bot):
     bot.add_cog(GitHub(bot))
