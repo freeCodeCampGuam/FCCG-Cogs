506ee302033974a6fb75f52278b7b7c0881c8744
Chovin <Chovin@users.noreply.github.com>
2017-08-03 00:28:23 +1000

    distribset file and toggle

--- keydistrib/keydistrib.py
+++ keydistrib/keydistrib.py
@@ -1,4 +1,5 @@
 import discord
+import red
 from discord.ext import commands
 from cogs.utils.dataIO import dataIO
 from cogs.utils import checks
@@ -12,18 +13,44 @@ SETTINGS_PATH = "data/keydistrib/settings.json"
 
 
 #TODO: 1st phase
+#TODO: get file from owner
+#TODO:  if from file, reindex on file modification date change
 #TODO: give a key from admin/mod cmd via PM
 #TOOD: only give on confirmation
 #TODO: track
-#TODO: 	if they were already given a key
-#TODO: 	if confirmed
-#TODO: 	who gave the key
-#TODO:	userinfo: name/id/date
+#TODO:  if they were already given a key
+#TODO:  if confirmed
+#TODO:  who gave the key
+#TODO:  userinfo: name/id/date
 
 #TODO: 2nd phase
 #TODO: hand out key on join from specific invite url
 #TODO: different files (key pools)
 #TODO: display user-key info. who has gotten what, etc
+#TODO: also get key-list in DM from mod/admin?
+#TODO: msg tied to each file/key (line override)
+
+
+#---- settings format -----
+# settings = {
+#     "FILES": {
+#         "filepath": {
+#             "SERVERS": ["sid"],
+#             "KEYS": {
+#                 "key": {
+#                     "STATUS": "UNUSED"/"USED",
+#                     "DATE": timestamp (update to last action)
+#                     "RECIPIENT": {"NAME": "bob", "UID": "uid"},
+#                     "SENDER": "uid"
+#                 }
+#             },
+#             "DATE_MODIFIED": timestamp
+#         }
+#     },
+#     "USERS": {
+#         "uid": ["filepath\nkey"]  # key indexes
+#     }
+# }
 
 
 class KeyDistrib:
@@ -33,14 +60,91 @@ class KeyDistrib:
         self.bot = bot
         self.settings = dataIO.load_json(SETTINGS_PATH)
 
+    def _save(self):
+        dataIO.save_json(SETTINGS_PATH, self.settings)
+
+    @checks.is_owner()
+    @commands.group(pass_context=True, no_pm=True)
+    async def distribset(self, ctx):
+        """#TODO: description"""
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+    
+    @checks.is_owner()
+    @distribset.command(pass_context=True, name="file", no_pm=True)
+    async def distribset_file(self, ctx, file_path):
+        """Set file to read keys from.
+        Relative from data/keydistrib/
+        Absolute filepaths work as well
+
+        #TODO: describe file format here"""
+        server = ctx.message.server
+        if not os.path.isabs(file_path):
+            file_path = 'data/keydistrib/' + file_path
+        if not os.path.exists(file_path):
+            #TODO: tell user file doesn't exist
+            return
+
+        with open(file_path) as f:
+            contents = f.read()
+        keys = filter(None, contents.splitlines())
+        mtime = os.path.getmtime(path)
+
+        keyring = self.settings["FILES"].setdefault(file_path, {
+            "SERVERS": [server.id],
+            "KEYS": {k: None for k in keys},
+            "DATE_MODIFIED": mtime
+        })
+        # what if our mtime is newer than file's?
+        #TODO: prompt user?
+        if mtime != keyring["DATE_MODIFIED"]:
+            # removes non-existing unused keys
+            # adds new keys
+            #TODO: write this
+            self._update_keys(file_path, keys)
+
+        if server.id not in keyring["SERVERS"]:
+            keyring["SERVERS"].append(server.id)
+
+        self._save()
+        #TODO: tell user it's done
+
+    @distribset.command(pass_context=True, name="toggle", no_pm=True)
+    async def distribset_toggle(self, ctx, file_path):
+        """#TODO: description"""
+        server = ctx.message.server
+
+        try:
+            keyring = self.settings["FILES"][file_path]
+        except KeyError:
+            await self.bot.say("That file has not been added yet.\n"
+                               "Add it with `{}distribset file`"
+                               .format(ctx.prefix))
+            return
+
+        try:
+            keyring["SERVERS"].remove(server.id)
+            msg = "Keys from that file can no longer be distributed in this server"
+        except ValueError:
+            keyring["SERVERS"].append(server.id)
+            msg = "Keys from that file can now be distributed in this server"
+
+        self._save()
+        await self.bot.reply(msg)
+
+
+
+
     @checks.mod_or_permissions()
     @commands.command(pass_context=True, no_pm=True)
-    async def givekey(self, ctx, ):
+    async def givekey(self, ctx, user: discord.Member):
         """description"""
         server = ctx.message.server
         channel = ctx.message.channel
         author = ctx.message.author
-        await self.bot.say("hi")
+        # send user confirmation prompt
+        await self.bot.whisper("")
+
 
 
 def check_folders():
@@ -68,7 +172,7 @@ def check_files():
             dataIO.save_json(SETTINGS_PATH, current)
 
 
-def setup(bot):
+def setup(bot: red.Bot):
     check_folders()
     check_files()
     n = KeyDistrib(bot)
