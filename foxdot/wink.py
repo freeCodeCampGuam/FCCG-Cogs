dfe0afbb7ad26392e7a25b6ca5868b51f99b2ccb
Chovin <Chovin@users.noreply.github.com>
2018-01-11 18:08:48 +1000

    adjust setup section for jamcord rename

--- foxdot/wink.py
+++ foxdot/wink.py
@@ -1028,17 +1028,17 @@ async def wait_for_reaction_remove(bot, emoji=None, *, user=None,
 
 
 def check_folders():
-    paths = ("data/foxdot", SAMPLE_PATH)
+    paths = ("data/jamcord", SAMPLE_PATH)
     for path in paths:
-      if not os.path.exists(path):
-          print("Creating {} folder...".format(path))
-          os.makedirs(path)
+        if not os.path.exists(path):
+            print("Creating {} folder...".format(path))
+            os.makedirs(path)
 
 def check_files():
     default = {"SAMPLES": {}, "INTERPRETER_PATHS": {"TROOP": None}}
 
     if not dataIO.is_valid_json(SETTINGS_PATH):
-        print("Creating default foxdot settings.json...")
+        print("Creating default jamcord settings.json...")
         dataIO.save_json(SETTINGS_PATH, default)
     else:  # consistency check
         current = dataIO.load_json(SETTINGS_PATH)
@@ -1047,11 +1047,12 @@ def check_files():
                 if key not in current.keys():
                     current[key] = default[key]
                     print(
-                        "Adding " + str(key) + " field to foxdot settings.json")
+                        "Adding " + str(key) + " field to jamcord settings.json")
             dataIO.save_json(SETTINGS_PATH, current)
+
 
 def setup(bot):
     check_folders()
     check_files()
-    n = Wink(bot)
+    n = Jamcord(bot)
     bot.add_cog(n)
